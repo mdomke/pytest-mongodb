c1361e0ae02d03919f0cd9433c51fb75af35323c
DUCLERT Nicolas <nicolas.duclert@safrangroup.com>
2017-11-13 17:01:34 +0100

    Build the path to the fixture directory from the config file path.
    Previously, this path was build from the launch path which leads to failing tests when launching from an IDE.

--- pytest_mongodb/plugin.py
+++ pytest_mongodb/plugin.py
@@ -82,7 +82,9 @@ def clean_database(db):
 
 
 def load_fixtures(db, config):
-    basedir = config.getoption('mongodb_fixture_dir') or config.getini('mongodb_fixture_dir')
+    config_dir = config.inifile.join('..')
+    fixture_dir = config.getoption('mongodb_fixture_dir') or config.getini('mongodb_fixture_dir')
+    basedir = str(config_dir.join(fixture_dir))
     fixtures = config.getini('mongodb_fixtures')
 
     for file_name in os.listdir(basedir):

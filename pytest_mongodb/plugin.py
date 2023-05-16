7469ddeef7a98620057e7d635d4f0d46cf58d5aa
Martin Domke <mail@martindomke.net>
2023-05-16 16:25:07 +0200

    Switch to PEP621 compliant project metadata configuration

--- pytest_mongodb/plugin.py
+++ pytest_mongodb/plugin.py
@@ -1,12 +1,13 @@
+import codecs
 import functools
 import json
 import os
-import codecs
 
-from bson import json_util
 import mongomock
 import pytest
 import yaml
+from bson import json_util
+
 
 try:
     import pymongo
@@ -17,60 +18,44 @@ except ImportError:
 
 
 _cache = {}
-_engine = 'mongomock'
+_engine = "mongomock"
 
 
 def pytest_addoption(parser):
+    parser.addini(name="mongodb_fixtures", help="Load these fixtures for tests", type="linelist")
     parser.addini(
-        name='mongodb_fixtures',
-        help='Load these fixtures for tests',
-        type='linelist')
-
+        name="mongodb_fixture_dir",
+        help="Try loading fixtures from this directory",
+        default=os.getcwd(),
+    )
     parser.addini(
-        name='mongodb_fixture_dir',
-        help='Try loading fixtures from this directory',
-        default=os.getcwd())
-
+        name="mongodb_engine", help="The database engine to use [mongomock]", default="mongomock"
+    )
     parser.addini(
-        name='mongodb_engine',
-        help='The database engine to use [mongomock]',
-        default='mongomock')
-
+        name="mongodb_host", help="The host where the mongodb-server runs", default="localhost"
+    )
     parser.addini(
-        name='mongodb_host',
-        help='The host where the mongodb-server runs',
-        default='localhost')
+        name="mongodb_dbname",
+        help="The name of the database where fixtures are created [pytest]",
+        default="pytest",
+    )
 
-    parser.addini(
-        name='mongodb_dbname',
-        help='The name of the database where fixtures are created [pytest]',
-        default='pytest')
-
+    parser.addoption("--mongodb-fixture-dir", help="Try loading fixtures from this directory")
+    parser.addoption("--mongodb-engine", help="The database engine to use [mongomock]")
+    parser.addoption("--mongodb-host", help="The host where the mongodb-server runs")
     parser.addoption(
-        '--mongodb-fixture-dir',
-        help='Try loading fixtures from this directory')
-
-    parser.addoption(
-        '--mongodb-engine',
-        help='The database engine to use [mongomock]')
-
-    parser.addoption(
-        '--mongodb-host',
-        help='The host where the mongodb-server runs')
-
-    parser.addoption(
-        '--mongodb-dbname',
-        help='The name of the database where fixtures are created [pytest]')
+        "--mongodb-dbname", help="The name of the database where fixtures are created [pytest]"
+    )
 
 
 def pytest_configure(config):
     global _engine
-    _engine = config.getoption('mongodb_engine') or config.getini('mongodb_engine')
+    _engine = config.getoption("mongodb_engine") or config.getini("mongodb_engine")
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def mongodb(pytestconfig):
-    dbname = pytestconfig.getoption('mongodb_dbname') or pytestconfig.getini('mongodb_dbname')
+    dbname = pytestconfig.getoption("mongodb_dbname") or pytestconfig.getini("mongodb_dbname")
     client = make_mongo_client(pytestconfig)
     db = client[dbname]
     clean_database(db)
@@ -79,9 +64,9 @@ def mongodb(pytestconfig):
 
 
 def make_mongo_client(config):
-    engine = config.getoption('mongodb_engine') or config.getini('mongodb_engine')
-    host = config.getoption('mongodb_host') or config.getini('mongodb_host')
-    if engine == 'pymongo':
+    engine = config.getoption("mongodb_engine") or config.getini("mongodb_engine")
+    host = config.getoption("mongodb_host") or config.getini("mongodb_host")
+    if engine == "pymongo":
         if not HAVE_PYMONGO:
             pytest.fail("PyMongo is not installed.")
         client = pymongo.MongoClient(host)
@@ -96,21 +81,21 @@ def clean_database(db):
 
 
 def list_collection_names(db):
-    if hasattr(db, 'list_collection_names'):
+    if hasattr(db, "list_collection_names"):
         return db.list_collection_names()
     return db.collection_names(include_system_collections=False)
 
 
 def load_fixtures(db, config):
-    basedir = config.getoption('mongodb_fixture_dir') or config.getini('mongodb_fixture_dir')
+    basedir = config.getoption("mongodb_fixture_dir") or config.getini("mongodb_fixture_dir")
     if not os.path.isabs(basedir):
         basedir = config.rootdir.join(basedir).strpath
-    fixtures = config.getini('mongodb_fixtures')
+    fixtures = config.getini("mongodb_fixtures")
 
     for file_name in os.listdir(basedir):
         collection, ext = os.path.splitext(os.path.basename(file_name))
-        file_format = ext.strip('.')
-        supported = file_format in ('json', 'yaml')
+        file_format = ext.strip(".")
+        supported = file_format in ("json", "yaml")
         selected = collection in fixtures if fixtures else True
         if selected and supported:
             path = os.path.join(basedir, file_name)
@@ -118,22 +103,22 @@ def load_fixtures(db, config):
 
 
 def load_fixture(db, collection, path, file_format):
-    if file_format == 'json':
+    if file_format == "json":
         loader = functools.partial(json.load, object_hook=json_util.object_hook)
-    elif file_format == 'yaml':
+    elif file_format == "yaml":
         loader = functools.partial(yaml.load, Loader=yaml.FullLoader)
     else:
         return
     try:
         docs = _cache[path]
     except KeyError:
-        with codecs.open(path, encoding='utf-8') as fp:
+        with codecs.open(path, encoding="utf-8") as fp:
             _cache[path] = docs = loader(fp)
     insert_many(db[collection], docs)
 
 
 def insert_many(collection, docs):
-    if hasattr(collection, 'insert_many'):
+    if hasattr(collection, "insert_many"):
         return collection.insert_many(docs)
     return collection.insert(docs)
 
--- tests/unit/test_plugin.py
+++ tests/unit/test_plugin.py
@@ -3,8 +3,8 @@ from pytest_mongodb import plugin
 
 def test_load(mongodb):
     collection_names = mongodb.list_collection_names()
-    assert 'players' in collection_names
-    assert 'championships' in collection_names
+    assert "players" in collection_names
+    assert "championships" in collection_names
     assert len(plugin._cache.keys()) == 2
     check_players(mongodb.players)
     check_championships(mongodb.championships)
@@ -12,15 +12,15 @@ def test_load(mongodb):
 
 def check_players(players):
     assert players.count_documents({}) == 2
-    check_keys_in_docs(players, ['name', 'surname', "position"])
-    manuel = players.find_one({'name': 'Manuel'})
-    assert manuel['surname'] == 'Neuer'
-    assert manuel['position'] == 'keeper'
+    check_keys_in_docs(players, ["name", "surname", "position"])
+    manuel = players.find_one({"name": "Manuel"})
+    assert manuel["surname"] == "Neuer"
+    assert manuel["position"] == "keeper"
 
 
 def check_championships(championships):
     assert championships.count_documents({}) == 3
-    check_keys_in_docs(championships, ['year', 'host', 'winner'])
+    check_keys_in_docs(championships, ["year", "host", "winner"])
 
 
 def check_keys_in_docs(collection, keys):
@@ -30,14 +30,12 @@ def check_keys_in_docs(collection, keys):
 
 
 def test_insert(mongodb):
-    mongodb.players.insert_one({
-        'name': 'Bastian',
-        'surname': 'Schweinsteiger',
-        'position': 'midfield'
-    })
+    mongodb.players.insert_one(
+        {"name": "Bastian", "surname": "Schweinsteiger", "position": "midfield"}
+    )
     assert mongodb.players.count_documents({}) == 3
-    assert mongodb.players.find_one({'name': 'Bastian'})
+    assert mongodb.players.find_one({"name": "Bastian"})
 
 
 def test_mongo_engine(pytestconfig):
-    assert plugin.mongo_engine() == 'mongomock'
+    assert plugin.mongo_engine() == "mongomock"
